83db0a209e1f7b84a99a7f702314e9bc8d272d72
Roidon <roidon1999@gmail.com>
2026-06-21 18:00:26 +0530

    changes

--- URL_Short/index.js
+++ URL_Short/index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const URL = require("./models/url");
+const path=require("path")
 const app = express();
 const urlroute = require("./routes/url")
 const { connectMongoDB } = require("./connect")
 app.use(express.json()); // <-- add this
+app.set("view engine","ejs")
+app.set("views",path.resolve("./views"))
 //connection
 connectMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => console.log("connection is successfull"));
 app.use("/url", urlroute)
 
+app.get("/test",async(req,res)=>{
+    let info =await URL.find({})
+    console.log(info,"info")
+    // res.send(`<h1>{info[0].shortId}</h1>`)
+    res.render("home",{urls:info})
+})
+
 app.get("/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
-
     console.log("Requested shortId:", shortId);
-
     const entry = await URL.findOneAndUpdate(
         { shortId },
         {
